03c5280ef417da0a76cc3a6cfb3dee79967ca1b5
kinggtree <ghoststeve2017@outlook.com>
2023-10-31 17:49:58 +0800

    fixed editEnv.sh bugs

--- editEnv.sh
+++ editEnv.sh
@@ -8,12 +8,12 @@ fi
 
 # 动态获取公网IP并设置为环境变量
 public_ip=$(curl -s ifconfig.me)
-echo "export EXPRESS_API_BASE_URL='http://$public_ip:5000'" >> /etc/environment
+echo "export EXPRESS_API_BASE_URL='$public_ip:5000'" >> /etc/environment
 source /etc/environment
 
 # 修改.env.production文件中的REACT_APP_API_BASE_URL变量
 env_file="/root/ChatRoom/my-app/.env.production"
-sed -i "/^REACT_APP_API_BASE_URL=/c\REACT_APP_API_BASE_URL=$public_ip:5000" $env_file
+sed -i "/^REACT_APP_API_BASE_URL=/c\REACT_APP_API_BASE_URL=http://$public_ip:5000" $env_file
 
 echo $EXPRESS_API_BASE_URL
 
